5767bbc7ffd59ed63b0f42ee83b340673c1f307e
walissonvb <walissonvb@gmail.com>
2026-04-08 18:33:31 -0300

    config  commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  geminiApiKey : "AIzaSyCCM2tgO0Yup6ZVXJTkkUjQqfON-BgXC0M",
+  firebaseConfig : {
+  apiKey: "AIzaSyD59nPl0NeX3lmQrDoOPiUnFXH5BSgJ53M",
+  authDomain: "prensa-caixa.firebaseapp.com",
+  projectId: "prensa-caixa",
+  storageBucket: "prensa-caixa.firebasestorage.app",
+  messagingSenderId: "928872223162",
+  appId: "1:928872223162:web:3b9211be7425bbd0774d83",
+  measurementId: "G-QNXV208VJH"
+},
+
 };
