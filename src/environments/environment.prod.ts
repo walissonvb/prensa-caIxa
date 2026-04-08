b73bd5d0da851ba6cf5694cdc82edaf8c596fa83
walissonvb <walissonvb@gmail.com>
2026-04-08 18:37:54 -0300

    config01  commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,27 @@
-export const environment = {
-  production: true,
-  geminiApiKey : "AIzaSyCCM2tgO0Yup6ZVXJTkkUjQqfON-BgXC0M",
-  firebaseConfig : {
-  apiKey: "AIzaSyD59nPl0NeX3lmQrDoOPiUnFXH5BSgJ53M",
-  authDomain: "prensa-caixa.firebaseapp.com",
-  projectId: "prensa-caixa",
-  storageBucket: "prensa-caixa.firebasestorage.app",
-  messagingSenderId: "928872223162",
-  appId: "1:928872223162:web:3b9211be7425bbd0774d83",
-  measurementId: "G-QNXV208VJH"
-},
+export interface Environment {
+  production: boolean;
+  geminiApiKey: string;
+  firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId?: string;
+  };
+}
 
+export const environment: Environment = {
+  production: false,
+  geminiApiKey: "AIzaSyCCM2tgO0Yup6ZVXJTkkUjQqfON-BgXC0M",
+  firebaseConfig: {
+    apiKey: "AIzaSyD59nPl0NeX3lmQrDoOPiUnFXH5BSgJ53M",
+    authDomain: "prensa-caixa.firebaseapp.com",
+    projectId: "prensa-caixa",
+    storageBucket: "prensa-caixa.firebasestorage.app",
+    messagingSenderId: "928872223162",
+    appId: "1:928872223162:web:3b9211be7425bbd0774d83",
+    measurementId: "G-QNXV208VJH"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,26 +1,27 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+export interface Environment {
+  production: boolean;
+  geminiApiKey: string;
+  firebaseConfig: {
+    apiKey: string;
+    authDomain: string;
+    projectId: string;
+    storageBucket: string;
+    messagingSenderId: string;
+    appId: string;
+    measurementId?: string;
+  };
+}
 
-export const environment = {
+export const environment: Environment = {
   production: false,
-  geminiApiKey : "AIzaSyCCM2tgO0Yup6ZVXJTkkUjQqfON-BgXC0M",
-  firebaseConfig : {
-  apiKey: "AIzaSyD59nPl0NeX3lmQrDoOPiUnFXH5BSgJ53M",
-  authDomain: "prensa-caixa.firebaseapp.com",
-  projectId: "prensa-caixa",
-  storageBucket: "prensa-caixa.firebasestorage.app",
-  messagingSenderId: "928872223162",
-  appId: "1:928872223162:web:3b9211be7425bbd0774d83",
-  measurementId: "G-QNXV208VJH"
-},
+  geminiApiKey: "AIzaSyCCM2tgO0Yup6ZVXJTkkUjQqfON-BgXC0M",
+  firebaseConfig: {
+    apiKey: "AIzaSyD59nPl0NeX3lmQrDoOPiUnFXH5BSgJ53M",
+    authDomain: "prensa-caixa.firebaseapp.com",
+    projectId: "prensa-caixa",
+    storageBucket: "prensa-caixa.firebasestorage.app",
+    messagingSenderId: "928872223162",
+    appId: "1:928872223162:web:3b9211be7425bbd0774d83",
+    measurementId: "G-QNXV208VJH"
+  }
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
